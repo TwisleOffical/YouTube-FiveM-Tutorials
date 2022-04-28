a1fd48e8daf34d962543c9206dc4e2b65335252f
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-04-28 19:51:07 -0400

    Add files via upload

---
+++ EP1-Commands/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Twisle'
+description 'EP1 Commands'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+
+-- Config
+shared_scripts {
+    'config.lua'
+}
